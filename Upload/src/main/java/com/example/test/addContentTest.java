e1b0ac135c1506d0fd042f73b970ce44a1a2b7a2
chuck <2233543613@qq.com>
2020-12-29 21:18:45 +0800

    tring to commit..

---
+++ Upload/src/main/java/com/example/test/addContentTest.java
@@ -0,0 +1,7 @@
+package com.example.test;
+
+public class addContentTest {
+    public static void main(String[] args) {
+        System.out.println("trying to add project new content to github...");
+    }
+}
